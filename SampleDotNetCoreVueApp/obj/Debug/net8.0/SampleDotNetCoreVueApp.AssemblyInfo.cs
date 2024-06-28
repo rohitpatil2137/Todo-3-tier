6d85e4e33daf6dbd7dff243131c675363e532438
Rohit <rohit.r@systoolsgroup.com>
2024-06-28 13:17:07 +0530

    dockerized the application, used docker-compose

--- SampleDotNetCoreVueApp/obj/Debug/net8.0/SampleDotNetCoreVueApp.AssemblyInfo.cs
+++ SampleDotNetCoreVueApp/obj/Debug/net8.0/SampleDotNetCoreVueApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleDotNetCoreVueApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9d7362ed7f17cbb3cc5ac5def66b194975752c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd8ffe84ea0c08f59c742469959977df4de84fbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleDotNetCoreVueApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SampleDotNetCoreVueApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
